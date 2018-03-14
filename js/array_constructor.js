16b04b669c93d5cd16c2e4cdad6ee981f6a886f4
Adriano Pequeno <adrian.pekeno@gmail.com>
2018-03-14 17:56:30 -0300

    arrays...

---
+++ js/array_constructor.js
@@ -0,0 +1,6 @@
+var color = new Array('White',
+    'Black',
+    'custom');
+
+var el = document.getElementById('color');
+el.innerHTML = color[0];
---
+++ js/array_listeral.js
@@ -0,0 +1,5 @@
+var colors;
+colors = ['branco', 'preto', 'custom'];
+
+var el = document.getElementById('colors');
+el.textContent = colors[0];
